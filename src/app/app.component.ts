fdbd5d1be8c03279b4721018547c5361be5112f5
Darío Salazar Leal <dario12_58@hotmail.com>
2023-08-11 17:55:00 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { NoticiaService } from './services/noticia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'news-angular';
+  loading = false;
+
+  listNoticias: any[] = [];
+
+  constructor(private noticiasService: NoticiaService) {}
+
+  buscarNoticias(parametros: any) {
+    this.loading = true;
+    this.listNoticias = [];
+
+    setTimeout(() => {
+      this.noticiasService.getNoticias(parametros).subscribe(
+        (data) => {
+          this.loading = false;
+          this.listNoticias = data.articles;
+        },
+        (error) => {
+          console.log(error);
+          this.loading = false;
+        }
+      );
+    }, 1000);
+  }
 }
